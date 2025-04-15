e121d30af5782f8aa08c0f1eb9f8c831f29d0929
pimi8522 <pimi8522@gmail.com>
2025-04-15 10:06:57 +0100

    added fds

--- libft.h
+++ libft.h
@@ -42,6 +42,10 @@ void *ft_memchr(const void *s, int c, size_t n);
 int ft_memcmp(const void *s1, const void *s2, size_t n);
 char *ft_substr(char const *s, unsigned int start, size_t len);
 char *ft_strjoin(char const *s1, char const *s2);
+void ft_putchar_fd(char c, int fd);
+void ft_putstr_fd(char *s, int fd);
+void ft_putendl_fd(char *s, int fd);
+void ft_putnbr_fd(int n, int fd);
 
 #endif
 

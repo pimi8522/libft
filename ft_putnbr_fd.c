862ca394c43b620df6fa5dcd24f0d35a69b98a73
pimi8522 <pimi8522@gmail.com>
2025-04-15 10:48:31 +0100

    added putnbvr

--- ft_putnbr_fd.c
+++ ft_putnbr_fd.c
@@ -6,13 +6,29 @@
 /*   By: miduarte <miduarte@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/14 14:56:02 by miduarte          #+#    #+#             */
-/*   Updated: 2025/04/14 14:56:27 by miduarte         ###   ########.fr       */
+/*   Updated: 2025/04/15 10:44:31 by miduarte         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void ft_putnbr_fd(int n, int fd)
+void ft_putnbr_fd(int nb, int fd)
 {
-	
+	if (nb == -2147483648)
+	{
+		ft_putchar('-');
+		ft_putchar('2');
+		ft_putnbr(147483648);
+	}
+	else
+	{
+		if (nb < 0)
+		{
+			ft_putchar_fd('-', fd);
+			nb = -nb;
+		}
+		if (nb >= 10)
+			ft_putnbr_fd(nb / 10, fd);
+		ft_putchar_fd(nb % 10 + '0', fd);
+	}
 }
--- libft.h
+++ libft.h
@@ -45,7 +45,7 @@ char *ft_strjoin(char const *s1, char const *s2);
 void ft_putchar_fd(char c, int fd);
 void ft_putstr_fd(char *s, int fd);
 void ft_putendl_fd(char *s, int fd);
-void ft_putnbr_fd(int n, int fd);
+void ft_putnbr_fd(int nb, int fd);
 
 #endif
 
